c71e900524853ea0ad820ed6afef49a53f738365
Alexey Naiden <alexey.nayden@gmail.com>
2022-05-07 22:57:37 +0300

    Working map

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,60 +1,77 @@
-<script context="module" lang="ts">
-	export const prerender = true;
-</script>
-
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+    import Panzoom from '@panzoom/panzoom'
+    import InlineSVG from 'svelte-inline-svg';
+    import map from '../data/Frame.svg';
+    import {onMount} from "svelte";
+    import locations from '../data/locations.json'
+
+
+    onMount(() => {
+        const width = 904;
+        const height = 844;
+        let startScale = 1;
+        if (width < height) {
+            startScale = window.innerWidth / width
+        } else {
+            startScale = window.innerHeight / height
+        }
+        const elem = document.getElementById('target-image')
+        const panzoom = Panzoom(elem, {
+            maxScale: 5,
+            contain: 'outside',
+            startX: -1 * Math.round(width / 2),
+            startY: -1 * Math.round(height / 2),
+            startScale
+        });
+        elem.parentElement.addEventListener('wheel', panzoom.zoomWithWheel)
+        elem.addEventListener('panzoomchange', (event) => {
+            // console.log(event.detail) // => { x: 0, y: 0, scale: 1 }
+        })
+    })
+
+    let searchTerm: string;
+    let candidates: string[] = [];
+
+    $: {
+        if (searchTerm) {
+            const normalizedTerm = searchTerm.trim().toLowerCase();
+            candidates = Object.keys(locations).filter((k: string) => k.toLowerCase().includes(normalizedTerm))
+        }
+    }
+
+    const showCampfire = (identifier: string) => {
+        searchTerm = '';
+        const campfireId = locations[identifier];
+        const mapSvg = document.getElementById('target-image')
+        mapSvg.scrollTo(254, 539);
+    }
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<div class="w-screen h-screen m-0 overflow-hidden">
+    <div class="absolute top-[3%] left-[5%] right-[5%] z-10">
+        <div class="w-full">
+            <input type="text"
+                   class="block opacity-70 bg-[#3C241D] focus:opacity-100 text-white rounded-2xl px-5 py-2 w-full text-[24px]"
+                   placeholder="Отряд или костёр..." bind:value={searchTerm}/>
+        </div>
+        {#if searchTerm && searchTerm.length >= 2}
+            {#if candidates && candidates.length > 0}
+                <div class="bg-[#3C241D] rounded-2xl px-5 py-2 w-full text-[24px] mt-5 text-white">
+                    {#each candidates as candidate}
+                        <div class="hover:brightness-50 cursor-pointer leading-relaxed"
+                             on:click={() => showCampfire(candidate)}>
+                            {candidate}
+                        </div>
+                    {/each}
+                </div>
+            {:else}
+                <div class="bg-[#3C241D] rounded-2xl px-5 py-2 w-full text-[24px] mt-5 text-[#FF6F6F]">
+                    На карте такого нет. Точно не ошибся в названии?
+                </div>
+            {/if}
+        {/if}
+    </div>
+    <div class="max-w-full max-h-full">
+        <InlineSVG src={map} id="target-image"/>
+    </div>
+</div>
